e72cf599f86883ee44302d5f79874134aed209e4
jstreuper <justin@burovoordeboeg.nl>
2024-03-13 15:53:27 +0100

    Added correct download link

--- Formula/gutenberg-blocks.rb
+++ Formula/gutenberg-blocks.rb
@@ -1,8 +1,8 @@
 class GutenbergBlocks < Formula
     desc "Script for instantiating and pulling Gutenberg blocks"
     homepage "https://github.com/burovoordeboeg/gutenberg-block-installer/"
-    url "https://github.com/burovoordeboeg/gutenberg-block-installer/archive/refs/tags/v0.0.2.tar.gz"
-    sha256 "cf7ef76a97bba410cf39f6fe27af87f5ddbbbc9a8f7b356576814aa07c2251c1"
+    url "https://github.com/burovoordeboeg/gutenberg-block-installer/blob/v0.0.2/versions/v0.0.2.tar.gz"
+    sha256 "8592c9c2215f2dc8f711adbbc46dcbc029edc35598c1b6453f9e14046ee99378"
     license "MIT"
   
     def install
