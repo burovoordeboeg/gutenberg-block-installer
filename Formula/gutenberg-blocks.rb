1eb2862e57001e6f91b8bb84e11d9db51a8f07ed
jstreuper <justin@burovoordeboeg.nl>
2024-03-13 15:54:46 +0100

    Removed checksum argument

--- Formula/gutenberg-blocks.rb
+++ Formula/gutenberg-blocks.rb
@@ -2,7 +2,6 @@ class GutenbergBlocks < Formula
     desc "Script for instantiating and pulling Gutenberg blocks"
     homepage "https://github.com/burovoordeboeg/gutenberg-block-installer/"
     url "https://github.com/burovoordeboeg/gutenberg-block-installer/blob/v0.0.2/versions/v0.0.2.tar.gz"
-    sha256 "8592c9c2215f2dc8f711adbbc46dcbc029edc35598c1b6453f9e14046ee99378"
     license "MIT"
   
     def install
