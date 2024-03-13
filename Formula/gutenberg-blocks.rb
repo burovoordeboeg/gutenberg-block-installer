58e17fbfe08c7a257d35733280bbfffd00c7a09f
jstreuper <justin@burovoordeboeg.nl>
2024-03-13 16:06:34 +0100

    Changed name for formula

--- Formula/gutenberg-blocks.rb
+++ Formula/gutenberg-blocks.rb
@@ -4,6 +4,7 @@ class GutenbergBlocks < Formula
     url "https://plugins.burovoordeboeg.nl/gutenberg-blocks.tar.gz"
     sha256 "e5f1b04195194c7d0ddc215d6ef68b659df79cd9"
     license "MIT"
+    version 0.0.2
   
     def install
       bin.install "gutenberg-blocks.sh" => "gutenberg-blocks"
