aff084455967b21671c134f12f48b91ee8fd58af
jstreuper <justin@burovoordeboeg.nl>
2024-03-13 16:10:29 +0100

    Added extra curl options

--- Formula/gutenberg-blocks.rb
+++ Formula/gutenberg-blocks.rb
@@ -1,12 +1,12 @@
 class GutenbergBlocks < Formula
     desc "Script for instantiating and pulling Gutenberg blocks"
     homepage "https://github.com/burovoordeboeg/gutenberg-block-installer/"
-    url "https://plugins.burovoordeboeg.nl/gutenberg-blocks.tar.gz"
     sha256 "e5f1b04195194c7d0ddc215d6ef68b659df79cd9"
     license "MIT"
     version "0.0.2"
   
     def install
+      system "curl", "-o", "gutenberg-blocks.tar.gz", "-A", "bvdb", "https://plugins.burovoordeboeg.nl/gutenberg-blocks.tar.gz"
       bin.install "gutenberg-blocks.sh" => "gutenberg-blocks"
     end
   end
