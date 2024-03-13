ce810447116f0bc52c55601ba8bbf24acb332fbd
jstreuper <justin@burovoordeboeg.nl>
2024-03-13 16:49:45 +0100

    Added option to get into the private repository

--- Formula/gutenberg-blocks.rb
+++ Formula/gutenberg-blocks.rb
@@ -1,12 +1,17 @@
 class GutenbergBlocks < Formula
   desc "Script for instantiating and pulling Gutenberg blocks"
   homepage "https://github.com/burovoordeboeg/gutenberg-block-installer"
-  url "https://github.com/burovoordeboeg/gutenberg-block-installer/archive/refs/tags/v0.0.3.zip"
+
+  credential_helper = "/usr/local/bin/git-credential-osxkeychain"
+  url "https://github.com/burovoordeboeg/gutenberg-block-installer.git",
+      using: :git, tag: "v0.0.4",
+      credential_helper: credential_helper
+
   license "MIT"
-  version: "0.0.3"
+  version "0.0.4"
 
   def install
     system "unzip", "-q", "gutenberg-blocks.zip"
-    bin.install "gutenberg-block-installer-0.0.3/scripts/gutenberg-blocks.sh" => "gutenberg-blocks"
+    bin.install "gutenberg-block-installer-0.0.4/scripts/gutenberg-blocks.sh" => "gutenberg-blocks"
   end
 end
