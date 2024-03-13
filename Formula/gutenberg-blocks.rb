6f5503256545521f46a09ffe0c7a76dbad09396f
jstreuper <justin@burovoordeboeg.nl>
2024-03-13 16:52:17 +0100

    Removed necessity to private repo's, fix for public repo

--- Formula/gutenberg-blocks.rb
+++ Formula/gutenberg-blocks.rb
@@ -3,15 +3,13 @@ class GutenbergBlocks < Formula
   homepage "https://github.com/burovoordeboeg/gutenberg-block-installer"
 
   credential_helper = "/usr/local/bin/git-credential-osxkeychain"
-  url "https://github.com/burovoordeboeg/gutenberg-block-installer.git",
-      using: :git, tag: "v0.0.4",
-      credential_helper: credential_helper
+  url "https://github.com/burovoordeboeg/gutenberg-block-installer.git"
 
   license "MIT"
-  version "0.0.4"
+  version "0.0.5"
 
   def install
     system "unzip", "-q", "gutenberg-blocks.zip"
-    bin.install "gutenberg-block-installer-0.0.4/scripts/gutenberg-blocks.sh" => "gutenberg-blocks"
+    bin.install "gutenberg-block-installer-0.0.5/scripts/gutenberg-blocks.sh" => "gutenberg-blocks"
   end
 end
