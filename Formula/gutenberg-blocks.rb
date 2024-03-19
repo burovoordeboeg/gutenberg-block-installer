3185d143a0fc2f868bb1907453b4f50ca8eb40af
jstreuper <justin@burovoordeboeg.nl>
2024-03-19 10:38:53 +0100

    Added version and help commands

--- Formula/gutenberg-blocks.rb
+++ Formula/gutenberg-blocks.rb
@@ -4,11 +4,43 @@ class GutenbergBlocks < Formula
   credential_helper = "/usr/local/bin/git-credential-osxkeychain"
   url "https://github.com/burovoordeboeg/homebrew-gutenberg-block-installer/"
   license "MIT"
-  version "0.0.9"
+  version "0.0.10"
 
   def install
-    system "curl", "-o", "gutenberg-blocks.zip", "-L", "https://github.com/burovoordeboeg/homebrew-gutenberg-block-installer/archive/refs/tags/0.0.9.zip"
+    system "curl", "-o", "gutenberg-blocks.zip", "-L", "https://github.com/burovoordeboeg/homebrew-gutenberg-block-installer/archive/refs/tags/0.0.10.zip"
     system "unzip", "-q", "gutenberg-blocks.zip"
-    bin.install "homebrew-gutenberg-block-installer-0.0.9/scripts/gutenberg-blocks.sh" => "gutenberg-blocks"
+    bin.install "homebrew-gutenberg-block-installer-0.0.10/scripts/gutenberg-blocks.sh" => "gutenberg-blocks"
+  end
+
+  # Function to display script version
+  def display_version
+    puts "Gutenberg Blocks Formula v#{version}"
+  end
+
+  # Function to display script help
+  def display_help
+    puts <<~HELP
+      Usage: gutenberg-blocks [options]
+
+      Options:
+        -v, --version   Display the version
+        -h, --help      Display this help message
+    HELP
+  end
+
+  # Main function to run the script
+  def run_script
+    if ARGV.include?("--version") || ARGV.include?("-v")
+      display_version
+    elsif ARGV.include?("--help") || ARGV.include?("-h")
+      display_help
+    else
+      # Add logic to run the script here
+    end
+  end
+
+  # Override the `prefix.install` method to invoke `run_script` after installation
+  def prefix.install
+    run_script
   end
 end
