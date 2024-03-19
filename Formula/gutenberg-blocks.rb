907dcd9a720b26d016cb792431002dcf829907f1
Justin Streuper <justin@burovoordeboeg.nl>
2024-03-19 10:22:45 +0100

    Update gutenberg-blocks.rb

--- Formula/gutenberg-blocks.rb
+++ Formula/gutenberg-blocks.rb
@@ -1,16 +1,14 @@
 class GutenbergBlocks < Formula
   desc "Script for instantiating and pulling Gutenberg blocks"
   homepage "https://github.com/burovoordeboeg/gutenberg-block-installer"
-
   credential_helper = "/usr/local/bin/git-credential-osxkeychain"
   url "https://github.com/burovoordeboeg/homebrew-gutenberg-block-installer/"
-
   license "MIT"
-  version "0.0.8"
+  version "0.0.9"
 
   def install
-    system "curl", "-o", "gutenberg-blocks.zip", "-L", "https://github.com/burovoordeboeg/homebrew-gutenberg-block-installer/archive/refs/tags/v0.0.8.zip"
+    system "curl", "-o", "gutenberg-blocks.zip", "-L", "https://github.com/burovoordeboeg/homebrew-gutenberg-block-installer/archive/refs/tags/0.0.9.zip"
     system "unzip", "-q", "gutenberg-blocks.zip"
-    bin.install "homebrew-gutenberg-block-installer-0.0.8/scripts/gutenberg-blocks.sh" => "gutenberg-blocks"
+    bin.install "homebrew-gutenberg-block-installer-0.0.9/scripts/gutenberg-blocks.sh" => "gutenberg-blocks"
   end
 end
